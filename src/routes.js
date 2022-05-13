3d2ce320e9d5306adacf6a93bc99d47b022bf465
Lightsoft123 <stevenfebrianto871@gmail.com>
2022-05-13 21:34:42 +0700

    Adding more elements in routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const boom = require('@hapi/boom');
-const { mainPage, mainPageCSS } = require('./handler');
+const { nanoid } = require('nanoid');
+const { mainPage, mainPageCSS, predictCSS, predictPage } = require('./handler');
 
+const data = [];
 const routes = [
     {
         method: 'GET',
@@ -15,11 +17,72 @@ const routes = [
     },
     {
         method: 'GET',
+        path: '/predict',
+        handler: predictPage,
+        options: {
+            files: {
+                relativeTo: path.join(__dirname, 'static')
+            }
+        }
+    },
+    {
+        method: 'POST',
+        path: '/predict',
+        handler: (request, h) => {
+            const image = request.payload;
+            const id = nanoid(16);
+            if(!image){
+                const response = h.response({
+                    status: 'fail',
+                    message: 'There is no image in it'
+                });
+                response.code(404);
+                return response;
+            }
+            const newImage = { id, image };
+            data.push(newImage);
+            const success = data.filter((d) => d.id === id).length > 0;
+            if(success){
+                const response = h.response({
+                    status: 'success',
+                    image: image,
+                    message: 'The image is uploaded successfully',
+                });
+                response.code(200);
+                return response;
+            }
+            const response = h.response({
+                status: 'fail',
+                message: 'You failed to appear the image'
+            })
+            response.code(500);
+            return response;
+        }
+    },
+    {
+        method: 'GET',
+        path: '/imageUploaded',
+        handler: (request, h) => {
+            return h.view('received_image', {image: "https://images.dog.ceo/breeds/kelpie/n02105412_1398.jpg"});
+        }
+    },
+    {
+        method: 'GET',
         path: '/index.css',
         handler: mainPageCSS,
         options: {
             files: {
                 relativeTo: path.join(__dirname, 'static'),
+            }
+        }
+    },
+    {
+        method: 'GET',
+        path: '/predict.css',
+        handler: predictCSS,
+        options: {
+            files: {
+                relativeTo: path.join(__dirname, 'static')
             }
         }
     },

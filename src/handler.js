9f07055f84147f1c84b42899637d38ce7a5b0fd1
Lightsoft123 <stevenfebrianto871@gmail.com>
2022-05-24 22:52:25 +0700

    Adding new Handler for new CSS page and for Upload file handler

--- src/handler.js
+++ src/handler.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 
 const mainPage = (request, h) => {
     return h.file("index.html");
@@ -17,4 +18,19 @@ const aboutPage = (request, h) => {
 const aboutPageCSS = (request, h) => {
     return h.file('About.css');
 }
-module.exports = { mainPage, mainPageCSS, predictPage, predictCSS, aboutPage, aboutPageCSS };
+const indexhbsCSS = (request, h) => {
+    return h.file('indexhbs.css');
+}
+const uploadImage = async (request, h) => {
+    const { payload } = request;
+    const filenames = payload.filename.hapi.filename;
+    const data = payload.filename._data;
+    return new Promise((resolve, reject) => fs.writeFile('src/Upload/' + filenames, data, err => {
+        if(err){
+            console.log(err);
+            reject(err)
+        }
+        resolve(h.view('received_image', {image: 'http://localhost:3000/predict/'+filenames}));
+    }))
+}
+module.exports = { mainPage, mainPageCSS, predictPage, predictCSS, aboutPage, aboutPageCSS, indexhbsCSS, uploadImage };

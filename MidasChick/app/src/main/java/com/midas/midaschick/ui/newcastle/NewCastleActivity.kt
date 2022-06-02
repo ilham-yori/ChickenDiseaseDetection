eba722463c40a82b9db31e844730150f8d72fe76
Ilham Yori P <ilhamyoripradana@gmail.com>
2022-06-02 20:29:36 +0700

    Added new icon & machine learning model

---
+++ MidasChick/app/src/main/java/com/midas/midaschick/ui/newcastle/NewCastleActivity.kt
@@ -0,0 +1,12 @@
+package com.midas.midaschick.ui.newcastle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.midas.midaschick.R
+
+class NewCastleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_castle)
+    }
+}

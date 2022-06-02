b1cfea83dca8286271d227f246d33edd568ffc45
ABBR\fy190 <fy190322@gmail.com>
2022-06-02 16:32:43 +0700

    fixing dashboard UI

--- MidasChick/app/src/main/java/com/midas/midaschick/ui/main/MainActivity.kt
+++ MidasChick/app/src/main/java/com/midas/midaschick/ui/main/MainActivity.kt
@@ -2,27 +2,16 @@ package com.midas.midaschick.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.midas.midaschick.R
-import com.midas.midaschick.ui.main.fragments.DashboardFragment
-import com.midas.midaschick.ui.main.fragments.HistoryFragment
-import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
-        navController = navHostFragment.navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        setupWithNavController(bottomNavigationView, navController)
     }
 
 }

699aa67a6aa5ab5a6b9d44067c71de7b67ea923e
ABBR\fy190 <fy190322@gmail.com>
2022-06-10 12:54:45 +0700

    detail modify

--- MidasChick/app/src/main/java/com/midas/midaschick/ui/coccidiosis/CoccidiosisActivity.kt
+++ MidasChick/app/src/main/java/com/midas/midaschick/ui/coccidiosis/CoccidiosisActivity.kt
@@ -5,20 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.midas.midaschick.databinding.ActivityCoccidiosisBinding
 import com.midas.midaschick.ui.main.MainActivity
+import kotlinx.android.synthetic.main.activity_coccidiosis.*
 
 
 class CoccidiosisActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCoccidiosisBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCoccidiosisBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         binding.btnHome.setOnClickListener {
             val move = Intent(this, MainActivity::class.java)
             startActivity(move)
         }
     }
+
+
 }
